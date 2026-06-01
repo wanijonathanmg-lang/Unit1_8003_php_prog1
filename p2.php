dc22443ec79c3e01dd0bcd03fdb78ead9f9e1f8a
wanijonathanmg-lang <wanijonathanmg@gmail.com>
2026-06-01 16:32:40 +0530

    Add files via upload

---
+++ p2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>max and min num</title>
+</head>
+<body>
+    
+<?php
+    $num=[25,17,9,36,16,85,6,54,58];
+
+    $max = max($num);
+    echo "Maximum number is: ".$max;
+    echo "<br>";
+    $min = min($num);
+    echo "Minimum number is: ".$min;
+?>
+</body>
+</html>
