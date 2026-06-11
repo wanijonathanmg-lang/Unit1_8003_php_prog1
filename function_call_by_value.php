97fcd46df4ba4756c4c929d8843214c806c23832
Jonathan Wani Mogga <wanijonathanmg@gmail.com>
2026-06-11 17:14:47 +0530

    Add files via upload

---
+++ function_call_by_value.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function adder($str2){
+          $str2 .= 'call by value';
+        }
+        $str = 'hello';
+        adder($str);
+        echo $str;
+    ?>
+</body>
+</html>
